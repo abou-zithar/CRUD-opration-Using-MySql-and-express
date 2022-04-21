d00a6aab20571908f24d9ccf1eef68c1e97be129
abou-zithar <dodiahli20@gmail.com>
2022-04-21 23:38:40 +0200

    second commit

--- DB/connection.js
+++ DB/connection.js
@@ -1,11 +1,11 @@
 const mysql2=require('mysql2')
-const Connection = require('mysql2/typings/mysql/lib/Connection')
 
-const mtSql=mysql2.createConnection({
+
+const mySql=mysql2.createConnection({
     host:'localhost',
     user:'root',
     database:'users',
     password:''
 })
 
-module.exports = mtSql
+module.exports = mySql
--- modules/users/controller/signup.controll.js
+++ modules/users/controller/signup.controll.js
@@ -1,5 +1,25 @@
+const MySql=require('../../../DB/connection')
+
+//  add user to sql database
 let adduser=(req,res)=>{
-    res.send('asadas')
+    let {name ,email,age,password,cpassword}=req.body;
+   if(password === cpassword){
+    MySql.execute(`select * from users where email = '${email}'`,(err,result)=>{
+        console.log(result);
+        if(result.length){
+            res.json({massage:'user already exist'})
+        }
+        else{
+            mySql.execute(`INSERT INTO users( name, email, age) VALUES ('${name}','${email}',${age})`,(err,data)=>{
+                console.log(data);
+            })
+            res.send('user added')
+        }
+    })
+   }else{
+       res.json({massage:'please check your password '})
+   }
+
 }
 
 
